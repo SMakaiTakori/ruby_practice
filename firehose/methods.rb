51c7354f32444263c73845c09c82d8e46e815ff8
Makai Takori <makait87@icloud.com>
2017-04-20 14:52:45 -0400

    practice with methods

---
+++ firehose/methods.rb
@@ -0,0 +1,59 @@
+# def double(num)
+#  return num * 2
+# end
+#
+# val = double(10)
+# puts val
+
+# * Practice with the Pythagorean theorem*
+
+# def pythagorean_theorem(a,b)
+#   a_squared = a * a
+#   b_squared = b * b
+#   a_squared_plus_b_squared = a_squared + b_squared
+#
+#   return Math.sqrt(a_squared_plus_b_squared)
+#
+#
+# end
+#
+# puts pythagorean_theorem(5, 12)
+
+# Cont'd-refactoring original code*******************
+#
+# def square(n)
+#   return n * n
+# end
+#
+# def pythagorean_theorem(a,b)
+# a_squared = square(a)
+# b_squared = square(b)
+# a_squared_plus_b_squared = a_squared + b_squared
+#
+#   return Math.sqrt(a_squared_plus_b_squared)
+# end
+#
+# puts pythagorean_theorem(5, 12)
+
+# *******Lesson Challenge--convert pounds to kg*****
+
+def convert(num)
+  return num = num * 0.453592
+end
+
+puts convert(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
